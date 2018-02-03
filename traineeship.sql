2e2e2c86cb7817f79c1a9303ebd3fc8e67d86cdf
Aleksey Ksenzov <aksenzov@gmail.com>
2018-02-03 12:24:04 +0700

    MySQL script without data

---
+++ traineeship.sql
@@ -0,0 +1,169 @@
+-- phpMyAdmin SQL Dump
+-- version 4.4.15.10
+-- https://www.phpmyadmin.net
+--
+-- Хост: localhost
+-- Время создания: Фев 01 2018 г., 03:39
+-- Версия сервера: 5.5.56-MariaDB
+-- Версия PHP: 5.4.16
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- База данных: `traineeship`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Структура таблицы `keywords`
+--
+
+CREATE TABLE IF NOT EXISTS `keywords` (
+  `id` int(11) NOT NULL,
+  `name` varchar(2048) NOT NULL,
+  `person_id` int(11) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+-- --------------------------------------------------------
+
+--
+-- Структура таблицы `pages`
+--
+
+CREATE TABLE IF NOT EXISTS `pages` (
+  `id` int(11) NOT NULL,
+  `url` varchar(2048) NOT NULL,
+  `site_id` int(11) NOT NULL,
+  `found_date_time` datetime NOT NULL,
+  `last_scan_date` datetime NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+-- --------------------------------------------------------
+
+--
+-- Структура таблицы `persons`
+--
+
+CREATE TABLE IF NOT EXISTS `persons` (
+  `id` int(11) NOT NULL,
+  `name` varchar(2048) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+-- --------------------------------------------------------
+
+--
+-- Структура таблицы `person_page_rank`
+--
+
+CREATE TABLE IF NOT EXISTS `person_page_rank` (
+  `person_id` int(11) NOT NULL,
+  `page_id` int(11) NOT NULL,
+  `rank` int(11) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+-- --------------------------------------------------------
+
+--
+-- Структура таблицы `sites`
+--
+
+CREATE TABLE IF NOT EXISTS `sites` (
+  `id` int(11) NOT NULL,
+  `name` varchar(256) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+--
+-- Индексы сохранённых таблиц
+--
+
+--
+-- Индексы таблицы `keywords`
+--
+ALTER TABLE `keywords`
+  ADD PRIMARY KEY (`id`),
+  ADD KEY `person_id_idx` (`person_id`);
+
+--
+-- Индексы таблицы `pages`
+--
+ALTER TABLE `pages`
+  ADD PRIMARY KEY (`id`),
+  ADD KEY `SiteID_idx` (`site_id`);
+
+--
+-- Индексы таблицы `persons`
+--
+ALTER TABLE `persons`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Индексы таблицы `person_page_rank`
+--
+ALTER TABLE `person_page_rank`
+  ADD KEY `PersonID_idx` (`person_id`),
+  ADD KEY `PageID_idx` (`page_id`);
+
+--
+-- Индексы таблицы `sites`
+--
+ALTER TABLE `sites`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- AUTO_INCREMENT для сохранённых таблиц
+--
+
+--
+-- AUTO_INCREMENT для таблицы `keywords`
+--
+ALTER TABLE `keywords`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+--
+-- AUTO_INCREMENT для таблицы `pages`
+--
+ALTER TABLE `pages`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+--
+-- AUTO_INCREMENT для таблицы `persons`
+--
+ALTER TABLE `persons`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+--
+-- AUTO_INCREMENT для таблицы `sites`
+--
+ALTER TABLE `sites`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+--
+-- Ограничения внешнего ключа сохраненных таблиц
+--
+
+--
+-- Ограничения внешнего ключа таблицы `keywords`
+--
+ALTER TABLE `keywords`
+  ADD CONSTRAINT `PersonIDKeywords` FOREIGN KEY (`person_id`) REFERENCES `persons` (`id`) ON DELETE CASCADE ON UPDATE CASCADE;
+
+--
+-- Ограничения внешнего ключа таблицы `pages`
+--
+ALTER TABLE `pages`
+  ADD CONSTRAINT `SiteID` FOREIGN KEY (`site_id`) REFERENCES `sites` (`id`) ON DELETE CASCADE ON UPDATE CASCADE;
+
+--
+-- Ограничения внешнего ключа таблицы `person_page_rank`
+--
+ALTER TABLE `person_page_rank`
+  ADD CONSTRAINT `PersonIDRank` FOREIGN KEY (`person_id`) REFERENCES `persons` (`id`) ON DELETE CASCADE ON UPDATE CASCADE,
+  ADD CONSTRAINT `PageID` FOREIGN KEY (`page_id`) REFERENCES `pages` (`id`) ON DELETE CASCADE ON UPDATE CASCADE;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;

a96f2fdf40ae4aca93e21d3ee8cec03f1d969cde
KsenZ <aksenzov@gmail.com>
2018-02-20 14:46:16 +0700

    Add UNIQUE KEY for url in pages table

--- traineeship.sql
+++ traineeship.sql
@@ -1,20 +1,43 @@
+-- phpMyAdmin SQL Dump
+-- version 4.4.15.10
+-- https://www.phpmyadmin.net
+--
+-- Хост: localhost
+-- Время создания: Фев 20 2018 г., 07:45
+-- Версия сервера: 5.5.56-MariaDB
+-- Версия PHP: 5.4.16
+
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 SET time_zone = "+00:00";
+
 
 /*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
 /*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
 /*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
 /*!40101 SET NAMES utf8mb4 */;
 
+--
+-- База данных: `traineeship`
+--
 
-DROP TABLE IF EXISTS `keywords`;
+-- --------------------------------------------------------
+
+--
+-- Структура таблицы `keywords`
+--
+
 CREATE TABLE IF NOT EXISTS `keywords` (
   `id` int(11) NOT NULL,
   `name` varchar(255) NOT NULL,
   `person_id` int(11) DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
-DROP TABLE IF EXISTS `pages`;
+-- --------------------------------------------------------
+
+--
+-- Структура таблицы `pages`
+--
+
 CREATE TABLE IF NOT EXISTS `pages` (
   `id` int(11) NOT NULL,
   `url` varchar(255) NOT NULL,
@@ -23,13 +46,23 @@ CREATE TABLE IF NOT EXISTS `pages` (
   `last_scan_date` timestamp NULL DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
-DROP TABLE IF EXISTS `persons`;
+-- --------------------------------------------------------
+
+--
+-- Структура таблицы `persons`
+--
+
 CREATE TABLE IF NOT EXISTS `persons` (
   `id` int(11) NOT NULL,
   `name` varchar(255) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
-DROP TABLE IF EXISTS `person_page_rank`;
+-- --------------------------------------------------------
+
+--
+-- Структура таблицы `person_page_rank`
+--
+
 CREATE TABLE IF NOT EXISTS `person_page_rank` (
   `id` int(11) NOT NULL,
   `person_id` int(11) NOT NULL,
@@ -37,50 +70,104 @@ CREATE TABLE IF NOT EXISTS `person_page_rank` (
   `rank` int(11) DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
-DROP TABLE IF EXISTS `sites`;
+-- --------------------------------------------------------
+
+--
+-- Структура таблицы `sites`
+--
+
 CREATE TABLE IF NOT EXISTS `sites` (
   `id` int(11) NOT NULL,
   `name` varchar(255) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
+--
+-- Индексы сохранённых таблиц
+--
 
+--
+-- Индексы таблицы `keywords`
+--
 ALTER TABLE `keywords`
   ADD PRIMARY KEY (`id`),
   ADD KEY `person_id_idx` (`person_id`);
 
+--
+-- Индексы таблицы `pages`
+--
 ALTER TABLE `pages`
   ADD PRIMARY KEY (`id`),
+  ADD UNIQUE KEY `url` (`url`),
   ADD KEY `SiteID_idx` (`site_id`);
 
+--
+-- Индексы таблицы `persons`
+--
 ALTER TABLE `persons`
   ADD PRIMARY KEY (`id`);
 
+--
+-- Индексы таблицы `person_page_rank`
+--
 ALTER TABLE `person_page_rank`
   ADD PRIMARY KEY (`id`),
   ADD KEY `PersonID_idx` (`person_id`),
   ADD KEY `PageID_idx` (`page_id`);
 
+--
+-- Индексы таблицы `sites`
+--
 ALTER TABLE `sites`
   ADD PRIMARY KEY (`id`);
 
+--
+-- AUTO_INCREMENT для сохранённых таблиц
+--
 
+--
+-- AUTO_INCREMENT для таблицы `keywords`
+--
 ALTER TABLE `keywords`
   MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+--
+-- AUTO_INCREMENT для таблицы `pages`
+--
 ALTER TABLE `pages`
   MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+--
+-- AUTO_INCREMENT для таблицы `persons`
+--
 ALTER TABLE `persons`
   MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+--
+-- AUTO_INCREMENT для таблицы `person_page_rank`
+--
 ALTER TABLE `person_page_rank`
   MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+--
+-- AUTO_INCREMENT для таблицы `sites`
+--
 ALTER TABLE `sites`
   MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+--
+-- Ограничения внешнего ключа сохраненных таблиц
+--
 
+--
+-- Ограничения внешнего ключа таблицы `keywords`
+--
 ALTER TABLE `keywords`
   ADD CONSTRAINT `PersonIDKeywords` FOREIGN KEY (`person_id`) REFERENCES `persons` (`id`) ON DELETE CASCADE ON UPDATE CASCADE;
 
+--
+-- Ограничения внешнего ключа таблицы `pages`
+--
 ALTER TABLE `pages`
   ADD CONSTRAINT `SiteID` FOREIGN KEY (`site_id`) REFERENCES `sites` (`id`) ON DELETE CASCADE ON UPDATE CASCADE;
 
+--
+-- Ограничения внешнего ключа таблицы `person_page_rank`
+--
 ALTER TABLE `person_page_rank`
   ADD CONSTRAINT `PageID` FOREIGN KEY (`page_id`) REFERENCES `pages` (`id`) ON DELETE CASCADE ON UPDATE CASCADE,
   ADD CONSTRAINT `PersonIDRank` FOREIGN KEY (`person_id`) REFERENCES `persons` (`id`) ON DELETE CASCADE ON UPDATE CASCADE;
